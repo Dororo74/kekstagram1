316f559357fc1bcb55ae5d61e9f64e015567ac94
Dororo74 <tussska79@gmail.com>
2025-12-21 13:12:43 +0500

    function

--- js/function.js
+++ js/function.js
@@ -1,5 +1,16 @@
-function polindrom (word, maxLength) {
-return word.lenght <= maxLength;
+export function wordLength (word, maxLength) {
+  return word.lenght <= maxLength;
 }
 
-console.log(polindrom('ded', 3));
+export function polindrom (string) {
+  const normString = string.replaceAll().toUpperCase();
+  let newString = '';
+  for (let i = normString.length - 1; i >= 0; i--) {
+    newString += normString[i];
+
+  }
+  if (normString === newString){
+    return true;
+  }
+  return false;
+}
